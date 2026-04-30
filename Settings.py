41f5ac969f6eb659cbd9edb4d23ed6986542fa6c
jeffry68 <69055604+jeffry68@users.noreply.github.com>
2026-04-29 21:10:47 -0400

    Add files via upload
    Added an option that takes longer to do each stat to prevent being rate-limited

--- Settings.py
+++ Settings.py
@@ -3,14 +3,14 @@ import os
 
 SETTINGS_FILE = "settings.json"
 
-# Default values
 prefered_status = ""
 min_endo_per_plat = 0
 randomize_stats = False
+request_mode = "safe"
 
 
 def load_settings():
-    global prefered_status, min_endo_per_plat, randomize_stats
+    global prefered_status, min_endo_per_plat, randomize_stats, request_mode
 
     if not os.path.exists(SETTINGS_FILE):
         save_settings()
@@ -22,13 +22,15 @@ def load_settings():
         prefered_status = data.get("prefered_status", "")
         min_endo_per_plat = data.get("min_endo_per_plat", 0)
         randomize_stats = data.get("randomize_stats", False)
+        request_mode = data.get("request_mode", "safe")
 
 
 def save_settings():
     data = {
         "prefered_status": prefered_status,
         "min_endo_per_plat": min_endo_per_plat,
-        "randomize_stats": randomize_stats
+        "randomize_stats": randomize_stats,
+        "request_mode": request_mode
     }
 
     with open(SETTINGS_FILE, "w") as f:
